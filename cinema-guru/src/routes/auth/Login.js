82dd9c7566c6de68b2f693b0481098ca8d05fca5
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-08 23:28:45 +0000

    Creation of the Login component with input fields for the username and password, and a submit button in accordance with the specifications.

---
+++ cinema-guru/src/routes/auth/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './auth.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faKey } from '@fortawesome/free-solid-svg-icons';
+
+const Login = ({ username, password, setUsername, setPassword }) => {
+  return (
+    <form className="auth-form">
+      <div className='title-auth'>
+        <p>Sign in with your account</p>
+      </div>
+      <div className="input-container">
+        <label><FontAwesomeIcon icon={faUser} className="input-icon" /> Username:</label>
+        <input 
+          type="text" 
+          value={username} 
+          onChange={(e) => setUsername(e.target.value)} 
+        />
+      </div>
+      <div className="input-container">
+        <label><FontAwesomeIcon icon={faKey} className="input-icon" /> Password:</label>
+        <input 
+          type="password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+        />
+      </div>
+      <button type="submit" className='btn-auth'><FontAwesomeIcon icon={faKey} className="input-icon" /> Sign In</button>
+    </form>
+  );
+};
+
+Login.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
+};
+
+export default Login;
