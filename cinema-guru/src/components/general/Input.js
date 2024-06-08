0fefdb712451bf78cad7c77ea4bfffd3be70977d
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-08 20:29:50 +0000

    Creation of the Input component with attribute and event management.

--- cinema-guru/src/components/general/Input.js
+++ cinema-guru/src/components/general/Input.js
@@ -1,59 +1,35 @@
-import './general.css';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './general.css';
 
+const Input = ({ label, type, className, value, setValue, icon, inputAttributes }) => {
+  const handleInput = (event) => {
+    setValue(event.target.value);
+  };
 
-export default function Input(props) {
-  const {
-    label,
-    type,
-    className,
-    value,
-    setValue,
-    icon,
-    inputAttributes,
-    isPassword,
-    showPassword,
-    setShowPassword
-  } = props;
-
-    function handleInput(e) {
-      setValue(e.target.value);
-    }
-
-    return (
-      <div className={className}>
-        {icon && <FontAwesomeIcon icon={icon}/>}
-        <label>{label}</label>
-        {isPassword && <FontAwesomeIcon id='showPasswordIcon' icon={showPassword ? faEyeSlash : faEye} onClick={setShowPassword}/>}
-        <input
+  return (
+    <div className={`input-container ${className}`}>
+      {icon && <FontAwesomeIcon icon={icon} className="input-icon" />}
+      <label>{label}</label>
+      <input
         type={type}
         value={value}
         onChange={handleInput}
-        {...inputAttributes}/>
-      </div>
-    );
-}
+        {...inputAttributes}
+      />
+    </div>
+  );
+};
 
 Input.propTypes = {
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired,
+  className: PropTypes.string,
   value: PropTypes.any.isRequired,
   setValue: PropTypes.func.isRequired,
   icon: PropTypes.object,
   inputAttributes: PropTypes.object,
-  isPassword: PropTypes.bool,
-  showPassword: PropTypes.bool,
-  setShowPassword: PropTypes.func
-}
+};
 
-Input.defaultProps = {
-  icon: null,
-  inputAttributes: null,
-  isPassword: false,
-  showPassword: false,
-  setShowPassword: () => {}
-}
+export default Input;
