ae78a12ab104e249eda90f74c37408ecc07539cc
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-09 09:01:58 +0000

    Add search icon to SearchBar component

--- cinema-guru/src/components/general/SearchBar.js
+++ cinema-guru/src/components/general/SearchBar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './general.css';
 
 
-const SearchBar = ({ title, setTitle }) => {
+const SearchBar = ({ title, setTitle, icon }) => {
   const handleInput = (event) => {
     setTitle(event.target.value);
   };
 
   return (
     <div className="search-bar">
+      {icon && <FontAwesomeIcon icon={icon} className="search-icon" />}
       <input
         type="text"
         value={title}
@@ -23,6 +25,7 @@ const SearchBar = ({ title, setTitle }) => {
 SearchBar.propTypes = {
   title: PropTypes.string.isRequired,
   setTitle: PropTypes.func.isRequired,
+  icon: PropTypes.object,
 };
 
 export default SearchBar;
