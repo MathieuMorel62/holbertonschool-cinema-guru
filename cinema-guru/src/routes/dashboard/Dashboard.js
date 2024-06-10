6d0562cf8b6c54e1916c6c995423acf0ac0d5a6b
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-10 13:34:39 +0000

    Modification of the Dashboard component to include SideBar.

--- cinema-guru/src/routes/dashboard/Dashboard.js
+++ cinema-guru/src/routes/dashboard/Dashboard.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './dashboard.css';
 import Header from '../../components/navigation/Header';
+import SideBar from '../../components/navigation/SideBar';
 
 const Dashboard = ({ userUsername, setIsLoggedIn }) => {
   return (
     <div className="dashboard-container">
       <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+      <SideBar />
+      <div className="dashboard-content">
+        {/* Contenu du Dashboard */}
+      </div>
     </div>
   );
 };
