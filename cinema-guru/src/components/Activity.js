a2728da643da671d85333fd7ae136a4b930d0d46
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-13 13:12:52 +0000

    Update Activity component to format date using date-fns

--- cinema-guru/src/components/Activity.js
+++ cinema-guru/src/components/Activity.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { format } from 'date-fns';
 import './components.css';
 
-const Activity = ({ username, movieTitle, date }) => {
+
+const Activity = ({ username = 'Unknown', movieTitle = 'Unknown', date = 'Unknown' }) => {
+  const formattedDate = format(new Date(date), 'MMMM dd, yyyy');
+
   return (
     <li className="activity-item">
-      <p><span className="activity-username">{username}</span> added <span className="activity-movie">{movieTitle}</span> to watch later - {date}</p>
+      <p><span className="activity-username">{username}</span> added <span className="activity-movie">{movieTitle}</span> to watch later - {formattedDate}</p>
     </li>
   );
 };
 
-
 Activity.propTypes = {
-  username: PropTypes.string.isRequired,
-  movieTitle: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
+  username: PropTypes.string,
+  movieTitle: PropTypes.string,
+  date: PropTypes.string,
 };
 
 export default Activity;
