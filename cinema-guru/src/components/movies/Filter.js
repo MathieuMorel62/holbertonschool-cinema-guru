52c739c32208acbb87708cd73da91c3c8236ff44
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-13 13:14:39 +0000

    Add Filter component for movie filtering

---
+++ cinema-guru/src/components/movies/Filter.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './movies.css';
+import SearchBar from '../general/SearchBar';
+import Input from '../general/Input';
+import SelectInput from '../general/SelectInput';
+import Tag from './Tag';
+import PropTypes from 'prop-types';
+
+const Filter = ({ minYear, setMinYear, maxYear, setMaxYear, sort, setSort, genres, setGenres, title, setTitle }) => {
+  const genreList = ["action", "drama", "comedy", "biography", "romance", "thriller", "war", "history", "sport", "sci-fi", "documentary", "crime", "fantasy"];
+  const sortOptions = [
+    { label: 'Latest', value: 'latest' },
+    { label: 'Oldest', value: 'oldest' },
+    { label: 'Highest Rated', value: 'highestrated' },
+    { label: 'Lowest Rated', value: 'lowestrated' }
+  ];
+
+  return (
+    <div className="filter-container">
+      <div className="filter-left">
+        <div className="search-bar-container">
+          <SearchBar title={title} setTitle={setTitle} />
+        </div>
+        <div className='filter-sort'>
+          <Input type="number" value={minYear} setValue={setMinYear} label="Min Date:" />
+          <Input type="number" value={maxYear} setValue={setMaxYear} label="Max Date:" />
+          <SelectInput value={sort} setValue={setSort} options={sortOptions} label="Sort:" />
+        </div>
+      </div>
+      <div className="filter-right">
+        <div className="tags">
+          {genreList.map(genre => (
+            <Tag key={genre} genre={genre} filter={true} genres={genres} setGenres={setGenres} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  minYear: PropTypes.string.isRequired,
+  setMinYear: PropTypes.func.isRequired,
+  maxYear: PropTypes.string.isRequired,
+  setMaxYear: PropTypes.func.isRequired,
+  sort: PropTypes.string.isRequired,
+  setSort: PropTypes.func.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  setGenres: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  setTitle: PropTypes.func.isRequired,
+};
+
+export default Filter;
