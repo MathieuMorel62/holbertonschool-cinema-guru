a98280b3a33ec27a077181b7d7a15c98e2703413
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-13 13:18:01 +0000

    Update Dashboard component to use react-router for routing

--- cinema-guru/src/routes/dashboard/Dashboard.js
+++ cinema-guru/src/routes/dashboard/Dashboard.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './dashboard.css';
 import Header from '../../components/navigation/Header';
 import SideBar from '../../components/navigation/SideBar';
+import HomePage from './HomePage';
+import Favorites from './Favorites';
+import WatchLater from './WatchLater';
+import { Routes, Route } from 'react-router-dom';
+
 
 const Dashboard = ({ userUsername, setIsLoggedIn }) => {
   return (
@@ -9,7 +14,11 @@ const Dashboard = ({ userUsername, setIsLoggedIn }) => {
       <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
       <SideBar />
       <div className="dashboard-content">
-        {/* Contenu du Dashboard */}
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+        </Routes>
       </div>
     </div>
   );
