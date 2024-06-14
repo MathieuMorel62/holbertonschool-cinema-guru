9b542b448db5cc6845a20a7a5d4307aa0e30fd4a
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-06-14 14:07:48 +0000

    Update Activity component to display different activity messages based on activity type

--- cinema-guru/src/components/Activity.js
+++ cinema-guru/src/components/Activity.js
@@ -3,13 +3,32 @@ import PropTypes from 'prop-types';
 import { format } from 'date-fns';
 import './components.css';
 
-
-const Activity = ({ username = 'Unknown', movieTitle = 'Unknown', date = 'Unknown' }) => {
+const Activity = ({ username = 'Unknown', movieTitle = 'Unknown', date = 'Unknown', activityType = 'Unknown' }) => {
   const formattedDate = format(new Date(date), 'MMMM dd, yyyy');
+  let activityMessage;
+
+  switch (activityType) {
+    case 'favorite':
+      activityMessage = 'added to favorites';
+      break;
+    case 'watchLater':
+      activityMessage = 'added to watch later';
+      break;
+    case 'removeFavorited':
+      activityMessage = 'removed from favorites';
+      break;
+    case 'removeWatchLater':
+      activityMessage = 'removed from watch later';
+      break;
+    default:
+      activityMessage = 'performed an activity';
+  }
 
   return (
     <li className="activity-item">
-      <p><span className="activity-username">{username}</span> added <span className="activity-movie">{movieTitle}</span> to watch later - {formattedDate}</p>
+      <p>
+        <span className="activity-username">{username}</span> {activityMessage} <span className="activity-movie">{movieTitle}</span> - {formattedDate}
+      </p>
     </li>
   );
 };
@@ -18,6 +37,7 @@ Activity.propTypes = {
   username: PropTypes.string,
   movieTitle: PropTypes.string,
   date: PropTypes.string,
+  activityType: PropTypes.string,
 };
 
 export default Activity;
